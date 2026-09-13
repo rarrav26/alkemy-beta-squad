77a50dcb71fdc555597355b494c939160b05906e
matiliendogit <matiliendodev@gmail.com>
2026-09-13 14:33:46 -0300

    refactor(backend): eliminar IGenericRepository sin consumidores

--- backend/DigitlaArs.Api/Program.cs
+++ backend/DigitlaArs.Api/Program.cs
@@ -29,7 +29,6 @@ builder.Services.AddDbContext<DigitalArsDbContext>((sp, options) =>
     options.UseSqlServer(sp.GetRequiredService<SqlConnection>()));
 builder.Services.AddDbContext<AuthDbContext>((sp, options) =>
     options.UseSqlServer(sp.GetRequiredService<SqlConnection>()));
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ITipoMovimientoRepository, TipoMovimientoRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddIdentityCore<IdentityUser>(options =>
